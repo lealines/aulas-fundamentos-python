3e0d8729496b3e27e7c26c7535dfa8103a79e31f
lealines <ineslspinho@gmail.com>
2025-12-02 14:18:36 +0000

    Update ex076.py

--- Exercícios/ex076.py
+++ Exercícios/ex076.py
@@ -1,6 +1,4 @@
 def celsius_to_Fahrenheit(celsius:float):
     print(f"{celsius}ºC <=> {((celsius*1.8) + 32):.2f} F")
 
-
-
 celsius_to_Fahrenheit(float((input("Insira a temperatura em celsius: "))))
