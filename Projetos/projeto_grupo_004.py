a9ad7eca7306361fee5227adcda1c603911a167c
lealines <ineslspinho@gmail.com>
2025-12-05 16:17:54 +0000

    projeto grupo

---
+++ Projetos/projeto_grupo_004.py
@@ -0,0 +1,77 @@
+# Inês / Julia / Nádia
+
+from pathlib import Path
+
+pasta = Path('bloco_notas')
+ficheiro = pasta / 'notas.txt'
+
+def adicionar_nota():
+    nota = input('Nota:\n--> ')
+    with ficheiro.open('a', encoding='utf-8', errors='ignore') as file:
+        file.write(nota + '\n')
+    print('Nota adicionada!\n')
+
+
+def mostrar_notas():
+    notas = ficheiro.read_text(encoding='utf-8').strip()
+    print('\n--- Nota Gravadas ---')
+    print(notas if notas else '\nNenhuma nota encontrada.')
+
+
+def apagar_notas():
+    ficheiro.write_text('', encoding='utf-8', errors='ignore')
+    print('\nNotas apagadas!\n')
+
+
+def pesquisar_notas():
+    palavra = input('Palavra-chave:\n--> ').lower()
+    resultados = []
+
+    with ficheiro.open('r', encoding='utf-8', errors='ignore') as aberto:
+        for numero, linha in enumerate(aberto):
+            if palavra in linha.lower():
+                resultados.append((numero, linha.strip()))
+
+    aberto.close()
+
+    print('\n--- Pesquisa ---\n')
+
+
+    if resultados:
+        for numero, x in resultados:
+            print(x)
+    else:
+        print(f'\nNenhuma nota encontrada com a palavra {palavra}!')
+
+
+def mostrar_menu():
+    print('\n--- BLOCO DE NOTAS ---\n')
+    print('[ 1 ] - Adicionar nota')
+    print('[ 2 ] - Mostrar todas as notas')
+    print('[ 3 ] - Apagar todas as notas')
+    print('[ 4 ] - Pesquisar notas por palavra-chave')
+    print('[ 5 ] - Sair\n')
+
+
+def main():
+    while True:
+        mostrar_menu()
+        opcao = input('Escolha uma opção: ')
+
+        if opcao == '1':
+            adicionar_nota()
+        elif opcao == '2':
+            mostrar_notas()
+        elif opcao == '3':
+            apagar_notas()
+        elif opcao == '4':
+            pesquisar_notas()
+        elif opcao == '5':
+            print('A sair do programa...')
+            break
+        else:
+            print('\nOpção inválida! Tente novamente!\n')
+
+
+if __name__ == '__main__':
+    main()
